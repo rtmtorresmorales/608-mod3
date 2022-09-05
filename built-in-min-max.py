01592900838947ffa6fd8b73037c02e5466d986a
rtmtorresmorales <ratorres@ponce.inter.edu>
2022-09-05 10:23:11 -0400

    Update built-in-min-max.py

--- built-in-min-max.py
+++ built-in-min-max.py
@@ -1,1 +1,9 @@
-
+"""" Project 3 Ramon Torres"""
+DEF maximum(value1, value 2, value3)
+"""return the maximum of three values. ""
+max_value = value1
+IF value2 > max_value;
+max_value = value2
+IF value3 > max_value;
+max_value = value3
+RETURN max_value
