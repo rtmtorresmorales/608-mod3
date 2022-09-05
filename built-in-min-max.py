023f885c1ad35a2113e2ad341ce2bec9af9641db
rtmtorresmorales <ratorres@ponce.inter.edu>
2022-09-05 11:32:31 -0400

    Update built-in-min-max.py

--- built-in-min-max.py
+++ built-in-min-max.py
@@ -8,7 +8,7 @@ IF value3 > max_value;
 max_value = value3
 RETURN max_value
 PRINT (max_value)
-
+maximum(12, 27, 36)
 DEF minimum(value1, value 2, value3)
 """return the minum of three values. ""
 min_value = value1
@@ -18,3 +18,4 @@ IF value3 > min_value;
 min_value = value3
 RETURN min_value
 PRINT (min_value)
+minimum (15, 9, 27, 14)
